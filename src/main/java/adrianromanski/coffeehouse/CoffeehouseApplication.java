5f079b02d6d85bafa7879023bbcd1464ed95964d
Adrian Romański <muszynianka1337@gmail.com>
2020-04-06 19:55:44 +0200

    Initial commit

---
+++ src/main/java/adrianromanski/coffeehouse/CoffeehouseApplication.java
@@ -0,0 +1,13 @@
+package adrianromanski.coffeehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/adrianromanski/coffeehouse/CoffeehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package adrianromanski.coffeehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
